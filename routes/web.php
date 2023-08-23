400e53a8526f9a5d4eb75bc94cf1826b58a51e3c
AlbertoJTD <albertojtd@outlook.com>
2023-08-23 17:44:47 -0600

    Add request & query params

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function() {
+    return 'Hello world';
+});
+
+Route::get('/hello2', function() {
+    return response('<h1>Hello world</h1>', 200)
+            ->header('Content-Type', 'text/plain')
+            ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function($id) {
+    // dd($id);
+    // ddd($id);
+    return response('Post' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request) {
+    // dd($request); Get acces to the info
+
+    return "$request->name $request->city";
 });
