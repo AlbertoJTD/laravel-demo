17c37b919d554c0f5a87d1d150f6e14d1698dd75
AlbertoJTD <albertojtd@outlook.com>
2023-08-23 18:14:51 -0600

    Add blade example

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,22 @@
+<!-- Example using old syntax -> it's still useful -->
+{{-- <h1> <?php echo $heading ?></h1>
+
+<?php foreach ($listings as $listing): ?>
+  <h2><?php echo $listing['title'] ?></h2>
+  <p><?php echo $listing['description'] ?></p>
+<?php endforeach ?> --}}
+
+
+
+{{-- Example blade syntax --}}
+
+<h1> {{ $heading }} </h1>
+
+@if (count($listings) > 0)
+  <p>{{ count($listings)  }}</p>
+@endif
+
+@foreach ($listings as $listing)
+  <h2>{{ $listing['title'] }}</h2>
+  <p>{{ $listing['description'] }}</p>
+@endforeach
