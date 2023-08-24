30c2c79ab59c4edfca880a36a65a69feca936254
AlbertoJTD <albertojtd@outlook.com>
2023-08-23 18:02:51 -0600

    Pass date to views

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,56 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // view name, data
+    return view('listings', [
+        'heading' => 'Latest listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Testing 1',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Amet exercitationem dolores id molestiae cupiditate soluta aliquid quaerat
+                quasi vero error iste labore earum, temporibus repudiandae quod? Inventore
+                illum laboriosam repellat?'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Testing 2',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Amet exercitationem dolores id molestiae cupiditate soluta aliquid quaerat
+                quasi vero error iste labore earum, temporibus repudiandae quod? Inventore
+                illum laboriosam repellat?'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Testing 3',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Amet exercitationem dolores id molestiae cupiditate soluta aliquid quaerat
+                quasi vero error iste labore earum, temporibus repudiandae quod? Inventore
+                illum laboriosam repellat?'
+            ]
+        ]
+    ]);
 });
 
-Route::get('/hello', function() {
-    return 'Hello world';
-});
+// Route::get('/hello', function() {
+//     return 'Hello world';
+// });
 
-Route::get('/hello2', function() {
-    return response('<h1>Hello world</h1>', 200)
-            ->header('Content-Type', 'text/plain')
-            ->header('foo', 'bar');
-});
+// Route::get('/hello2', function() {
+//     return response('<h1>Hello world</h1>', 200)
+//             ->header('Content-Type', 'text/plain')
+//             ->header('foo', 'bar');
+// });
 
-Route::get('/posts/{id}', function($id) {
-    // dd($id);
-    // ddd($id);
-    return response('Post' . $id);
-})->where('id', '[0-9]+');
+// Route::get('/posts/{id}', function($id) {
+//     // dd($id);
+//     // ddd($id);
+//     return response('Post' . $id);
+// })->where('id', '[0-9]+');
 
-Route::get('/search', function(Request $request) {
-    // dd($request); Get acces to the info
+// Route::get('/search', function(Request $request) {
+//     // dd($request); Get acces to the info
 
-    return "$request->name $request->city";
-});
+//     return "$request->name $request->city";
+// });
