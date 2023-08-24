ae1b00f50b4b15d683996827d3926b5d390f5b16
AlbertoJTD <albertojtd@outlook.com>
2023-08-24 11:18:46 -0600

    Remove example

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,13 +1,3 @@
-<!-- Example using old syntax -> it's still useful -->
-{{-- <h1> <?php echo $heading ?></h1>
-
-<?php foreach ($listings as $listing): ?>
-  <h2><?php echo $listing['title'] ?></h2>
-  <p><?php echo $listing['description'] ?></p>
-<?php endforeach ?> --}}
-
-
-
 {{-- Example blade syntax --}}
 
 <h1> {{ $heading }} </h1>
