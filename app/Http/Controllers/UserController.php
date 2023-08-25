8f8a1a0e5955417d7874a8692f9abec80678ab98
AlbertoJTD <albertojtd@outlook.com>
2023-08-25 16:35:23 -0600

    Add user registration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function create() {
+        return view('users.register');
+    }
+
+    public function store(Request $request) {
+        $formsFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        // Hash password
+        $formsFields['password'] = bcrypt($formsFields['password']);
+        
+        // Create user
+        $user = User::create($formsFields);
+
+        // Login user
+        auth()->login($user);
+        return redirect('/')->with('message', 'User created and logged in. Welcome ' . $formsFields['name']);
+    }
+}
